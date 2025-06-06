409fe19a345b99c7258ef04ab4c8fc550f05bf9b
zhengzhou <zhengzhou@rivtower.com>
2025-06-06 14:13:06 +0800

    chore: 移动文件

--- src/view/com/pager/PagerWithHeader.tsx
+++ src/view/com/pager/PagerWithHeader.tsx
@@ -1,30 +1,12 @@
 import * as React from 'react'
-import {
-  LayoutChangeEvent,
-  NativeScrollEvent,
-  ScrollView,
-  StyleSheet,
-  View,
-} from 'react-native'
-import Animated, {
-  AnimatedRef,
-  runOnJS,
-  runOnUI,
-  scrollTo,
-  SharedValue,
-  useAnimatedRef,
-  useAnimatedStyle,
-  useSharedValue,
-} from 'react-native-reanimated'
+import {type ScrollView, View} from 'react-native'
+import {useAnimatedRef} from 'react-native-reanimated'
 
-import {useNonReactiveCallback} from '#/lib/hooks/useNonReactiveCallback'
-import {ScrollProvider} from '#/lib/ScrollContext'
-import {isIOS} from '#/platform/detection'
-import {Pager, PagerRef, RenderTabBarFnProps} from '#/view/com/pager/Pager'
-import {useTheme} from '#/alf'
-import {ListMethods} from '../util/List'
-import {PagerHeaderProvider} from './PagerHeaderContext'
-import {TabBar} from './TabBar'
+import {Pager, type PagerRef, type RenderTabBarFnProps} from '#/view/com/pager/Pager'
+import {atoms as a, web} from '#/alf'
+import * as Layout from '#/components/Layout'
+import {type ListMethods} from '../util/List'
+import {TabBar} from './TabBar.native'
 
 export interface PagerWithHeaderChildParams {
   headerHeight: number
@@ -42,12 +24,11 @@ export interface PagerWithHeaderProps {
   renderHeader?: ({
     setMinimumHeight,
   }: {
-    setMinimumHeight: (height: number) => void
+    setMinimumHeight: () => void
   }) => JSX.Element
   initialPage?: number
   onPageSelected?: (index: number) => void
   onCurrentPageSelected?: (index: number) => void
-  allowHeaderOverScroll?: boolean
 }
 export const PagerWithHeader = React.forwardRef<PagerRef, PagerWithHeaderProps>(
   function PageWithHeaderImpl(
@@ -60,127 +41,34 @@ export const PagerWithHeader = React.forwardRef<PagerRef, PagerWithHeaderProps>(
       initialPage,
       onPageSelected,
       onCurrentPageSelected,
-      allowHeaderOverScroll,
     }: PagerWithHeaderProps,
     ref,
   ) {
     const [currentPage, setCurrentPage] = React.useState(0)
-    const [tabBarHeight, setTabBarHeight] = React.useState(0)
-    const [headerOnlyHeight, setHeaderOnlyHeight] = React.useState(0)
-    const scrollY = useSharedValue(0)
-    const headerHeight = headerOnlyHeight + tabBarHeight
-
-    // capture the header bar sizing
-    const onTabBarLayout = useNonReactiveCallback((evt: LayoutChangeEvent) => {
-      const height = evt.nativeEvent.layout.height
-      if (height > 0) {
-        // The rounding is necessary to prevent jumps on iOS
-        setTabBarHeight(Math.round(height * 2) / 2)
-      }
-    })
-    const onHeaderOnlyLayout = useNonReactiveCallback((height: number) => {
-      if (height > 0) {
-        // The rounding is necessary to prevent jumps on iOS
-        setHeaderOnlyHeight(Math.round(height * 2) / 2)
-      }
-    })
 
     const renderTabBar = React.useCallback(
       (props: RenderTabBarFnProps) => {
         return (
-          <PagerHeaderProvider
-            scrollY={scrollY}
-            headerHeight={headerOnlyHeight}>
-            <PagerTabBar
-              headerOnlyHeight={headerOnlyHeight}
-              items={items}
-              isHeaderReady={isHeaderReady}
-              renderHeader={renderHeader}
-              currentPage={currentPage}
-              onCurrentPageSelected={onCurrentPageSelected}
-              onTabBarLayout={onTabBarLayout}
-              onHeaderOnlyLayout={onHeaderOnlyLayout}
-              onSelect={props.onSelect}
-              scrollY={scrollY}
-              testID={testID}
-              allowHeaderOverScroll={allowHeaderOverScroll}
-              dragProgress={props.dragProgress}
-              dragState={props.dragState}
-            />
-          </PagerHeaderProvider>
+          <PagerTabBar
+            items={items}
+            renderHeader={renderHeader}
+            isHeaderReady={isHeaderReady}
+            currentPage={currentPage}
+            onCurrentPageSelected={onCurrentPageSelected}
+            onSelect={props.onSelect}
+            tabBarAnchor={props.tabBarAnchor}
+            testID={testID}
+          />
         )
       },
       [
-        headerOnlyHeight,
         items,
         isHeaderReady,
         renderHeader,
         currentPage,
         onCurrentPageSelected,
-        onTabBarLayout,
-        onHeaderOnlyLayout,
-        scrollY,
         testID,
-        allowHeaderOverScroll,
       ],
-    )
-
-    const scrollRefs = useSharedValue<Array<AnimatedRef<any> | null>>([])
-    const registerRef = React.useCallback(
-      (scrollRef: AnimatedRef<any> | null, atIndex: number) => {
-        scrollRefs.modify(refs => {
-          'worklet'
-          refs[atIndex] = scrollRef
-          return refs
-        })
-      },
-      [scrollRefs],
-    )
-
-    const lastForcedScrollY = useSharedValue(0)
-    const adjustScrollForOtherPages = () => {
-      'worklet'
-      const currentScrollY = scrollY.get()
-      const forcedScrollY = Math.min(currentScrollY, headerOnlyHeight)
-      if (lastForcedScrollY.get() !== forcedScrollY) {
-        lastForcedScrollY.set(forcedScrollY)
-        const refs = scrollRefs.get()
-        for (let i = 0; i < refs.length; i++) {
-          const scollRef = refs[i]
-          if (i !== currentPage && scollRef != null) {
-            scrollTo(scollRef, 0, forcedScrollY, false)
-          }
-        }
-      }
-    }
-
-    const throttleTimeout = React.useRef<ReturnType<typeof setTimeout> | null>(
-      null,
-    )
-    const queueThrottledOnScroll = useNonReactiveCallback(() => {
-      if (!throttleTimeout.current) {
-        throttleTimeout.current = setTimeout(() => {
-          throttleTimeout.current = null
-          runOnUI(adjustScrollForOtherPages)()
-        }, 80 /* Sync often enough you're unlikely to catch it unsynced */)
-      }
-    })
-
-    const onScrollWorklet = React.useCallback(
-      (e: NativeScrollEvent) => {
-        'worklet'
-        const nextScrollY = e.contentOffset.y
-        // HACK: onScroll is reporting some strange values on load (negative header height).
-        // Highly improbable that you'd be overscrolled by over 400px -
-        // in fact, I actually can't do it, so let's just ignore those. -sfn
-        const isPossiblyInvalid =
-          headerHeight > 0 && Math.round(nextScrollY * 2) / 2 === -headerHeight
-        if (!isPossiblyInvalid) {
-          scrollY.set(nextScrollY)
-          runOnJS(queueThrottledOnScroll)()
-        }
-      },
-      [scrollY, queueThrottledOnScroll, headerHeight],
     )
 
     const onPageSelectedInner = React.useCallback(
@@ -201,19 +89,15 @@ export const PagerWithHeader = React.forwardRef<PagerRef, PagerWithHeaderProps>(
         {toArray(children)
           .filter(Boolean)
           .map((child, i) => {
-            const isReady =
-              isHeaderReady && headerOnlyHeight > 0 && tabBarHeight > 0
+            const isReady = isHeaderReady
             return (
-              <View key={i} collapsable={false}>
-                <PagerItem
-                  headerHeight={headerHeight}
-                  index={i}
-                  isReady={isReady}
-                  isFocused={i === currentPage}
-                  onScrollWorklet={i === currentPage ? onScrollWorklet : noop}
-                  registerRef={registerRef}
-                  renderTab={child}
-                />
+              <View
+                key={i}
+                collapsable={false}
+                style={{
+                  display: isReady ? undefined : 'none',
+                }}>
+                <PagerItem isFocused={i === currentPage} renderTab={child} />
               </View>
             )
           })}
@@ -224,166 +108,75 @@ export const PagerWithHeader = React.forwardRef<PagerRef, PagerWithHeaderProps>(
 
 let PagerTabBar = ({
   currentPage,
-  headerOnlyHeight,
-  isHeaderReady,
   items,
-  scrollY,
+  isHeaderReady,
   testID,
   renderHeader,
-  onHeaderOnlyLayout,
-  onTabBarLayout,
   onCurrentPageSelected,
   onSelect,
-  allowHeaderOverScroll,
-  dragProgress,
-  dragState,
+  tabBarAnchor,
 }: {
   currentPage: number
-  headerOnlyHeight: number
-  isHeaderReady: boolean
   items: string[]
   testID?: string
-  scrollY: SharedValue<number>
   renderHeader?: ({
     setMinimumHeight,
   }: {
-    setMinimumHeight: (height: number) => void
+    setMinimumHeight: () => void
   }) => JSX.Element
-  onHeaderOnlyLayout: (height: number) => void
-  onTabBarLayout: (e: LayoutChangeEvent) => void
+  isHeaderReady: boolean
   onCurrentPageSelected?: (index: number) => void
   onSelect?: (index: number) => void
-  allowHeaderOverScroll?: boolean
-  dragProgress: SharedValue<number>
-  dragState: SharedValue<'idle' | 'dragging' | 'settling'>
+  tabBarAnchor?: JSX.Element | null | undefined
 }): React.ReactNode => {
-  const t = useTheme()
-  const [minimumHeaderHeight, setMinimumHeaderHeight] = React.useState(0)
-  const headerTransform = useAnimatedStyle(() => {
-    const translateY =
-      Math.min(
-        scrollY.get(),
-        Math.max(headerOnlyHeight - minimumHeaderHeight, 0),
-      ) * -1
-    return {
-      transform: [
-        {
-          translateY: allowHeaderOverScroll
-            ? translateY
-            : Math.min(translateY, 0),
-        },
-      ],
-    }
-  })
-  const headerRef = React.useRef(null)
   return (
-    <Animated.View
-      pointerEvents={isIOS ? 'auto' : 'box-none'}
-      style={[styles.tabBarMobile, headerTransform, t.atoms.bg]}>
-      <View
-        ref={headerRef}
-        pointerEvents={isIOS ? 'auto' : 'box-none'}
-        collapsable={false}>
-        {renderHeader?.({setMinimumHeight: setMinimumHeaderHeight})}
-        {
-          // It wouldn't be enough to place `onLayout` on the parent node because
-          // this would risk measuring before `isHeaderReady` has turned `true`.
-          // Instead, we'll render a brand node conditionally and get fresh layout.
-          isHeaderReady && (
-            <View
-              // It wouldn't be enough to do this in a `ref` of an effect because,
-              // even if `isHeaderReady` might have turned `true`, the associated
-              // layout might not have been performed yet on the native side.
-              onLayout={() => {
-                // @ts-ignore
-                headerRef.current?.measure(
-                  (_x: number, _y: number, _width: number, height: number) => {
-                    onHeaderOnlyLayout(height)
-                  },
-                )
-              }}
-            />
-          )
-        }
-      </View>
-      <View
-        onLayout={onTabBarLayout}
-        style={{
-          // Render it immediately to measure it early since its size doesn't depend on the content.
-          // However, keep it invisible until the header above stabilizes in order to prevent jumps.
-          opacity: isHeaderReady ? 1 : 0,
-          pointerEvents: isHeaderReady ? 'auto' : 'none',
-        }}>
+    <>
+      <Layout.Center>{renderHeader?.({setMinimumHeight: noop})}</Layout.Center>
+      {tabBarAnchor}
+      <Layout.Center
+        style={[
+          a.z_10,
+          web([
+            a.sticky,
+            {
+              top: 0,
+              display: isHeaderReady ? undefined : 'none',
+            },
+          ]),
+        ]}>
         <TabBar
           testID={testID}
           items={items}
           selectedPage={currentPage}
           onSelect={onSelect}
           onPressSelected={onCurrentPageSelected}
-          dragProgress={dragProgress}
-          dragState={dragState}
+          dragProgress={undefined as any /* native-only */}
+          dragState={undefined as any /* native-only */}
         />
-      </View>
-    </Animated.View>
+      </Layout.Center>
+    </>
   )
 }
 PagerTabBar = React.memo(PagerTabBar)
 
 function PagerItem({
-  headerHeight,
-  index,
-  isReady,
   isFocused,
-  onScrollWorklet,
   renderTab,
-  registerRef,
 }: {
-  headerHeight: number
-  index: number
   isFocused: boolean
-  isReady: boolean
-  registerRef: (scrollRef: AnimatedRef<any> | null, atIndex: number) => void
-  onScrollWorklet: (e: NativeScrollEvent) => void
   renderTab: ((props: PagerWithHeaderChildParams) => JSX.Element) | null
 }) {
   const scrollElRef = useAnimatedRef()
-
-  React.useEffect(() => {
-    registerRef(scrollElRef, index)
-    return () => {
-      registerRef(null, index)
-    }
-  }, [scrollElRef, registerRef, index])
-
-  if (!isReady || renderTab == null) {
+  if (renderTab == null) {
     return null
   }
-
-  return (
-    <ScrollProvider onScroll={onScrollWorklet}>
-      {renderTab({
-        headerHeight,
-        isFocused,
-        scrollElRef: scrollElRef as React.MutableRefObject<
-          ListMethods | ScrollView | null
-        >,
-      })}
-    </ScrollProvider>
-  )
-}
-
-const styles = StyleSheet.create({
-  tabBarMobile: {
-    position: 'absolute',
-    zIndex: 1,
-    top: 0,
-    left: 0,
-    width: '100%',
-  },
-})
-
-function noop() {
-  'worklet'
+  return renderTab({
+    headerHeight: 0,
+    isFocused,
+    scrollElRef: scrollElRef as React.MutableRefObject<
+      ListMethods | ScrollView | null
+    >,
+  })
 }
 
 function toArray<T>(v: T | T[]): T[] {
@@ -392,3 +185,5 @@ function toArray<T>(v: T | T[]): T[] {
   }
   return [v]
 }
+
+function noop() {}
---
+++ src/view/com/pager/TabBar.native.tsx
@@ -0,0 +1,474 @@
+import {useCallback} from 'react'
+import {
+  type LayoutChangeEvent,
+  ScrollView,
+  StyleSheet,
+  View,
+} from 'react-native'
+import Animated, {
+  interpolate,
+  runOnJS,
+  runOnUI,
+  scrollTo,
+  type SharedValue,
+  useAnimatedReaction,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated'
+
+import {PressableWithHover} from '#/view/com/util/PressableWithHover'
+import {BlockDrawerGesture} from '#/view/shell/BlockDrawerGesture'
+import {atoms as a, useTheme} from '#/alf'
+import {Text} from '#/components/Typography'
+
+export interface TabBarProps {
+  testID?: string
+  selectedPage: number
+  items: string[]
+  onSelect?: (index: number) => void
+  onPressSelected?: (index: number) => void
+  dragProgress: SharedValue<number>
+  dragState: SharedValue<'idle' | 'dragging' | 'settling'>
+}
+
+const ITEM_PADDING = 10
+const CONTENT_PADDING = 6
+// How much of the previous/next item we're requiring
+// when deciding whether to scroll into view on tap.
+const OFFSCREEN_ITEM_WIDTH = 20
+
+export function TabBar({
+  testID,
+  selectedPage,
+  items,
+  onSelect,
+  onPressSelected,
+  dragProgress,
+  dragState,
+}: TabBarProps) {
+  const t = useTheme()
+  const scrollElRef = useAnimatedRef<ScrollView>()
+  const syncScrollState = useSharedValue<'synced' | 'unsynced' | 'needs-sync'>(
+    'synced',
+  )
+  const didInitialScroll = useSharedValue(false)
+  const contentSize = useSharedValue(0)
+  const containerSize = useSharedValue(0)
+  const scrollX = useSharedValue(0)
+  const layouts = useSharedValue<{x: number; width: number}[]>([])
+  const textLayouts = useSharedValue<{width: number}[]>([])
+  const itemsLength = items.length
+
+  const scrollToOffsetJS = useCallback(
+    (x: number) => {
+      scrollElRef.current?.scrollTo({
+        x,
+        y: 0,
+        animated: true,
+      })
+    },
+    [scrollElRef],
+  )
+
+  const indexToOffset = useCallback(
+    (index: number) => {
+      'worklet'
+      const layout = layouts.get()[index]
+      const availableSize = containerSize.get() - 2 * CONTENT_PADDING
+      if (!layout) {
+        // Should not happen, but fall back to equal sizes.
+        const offsetPerPage = contentSize.get() - availableSize
+        return (index / (itemsLength - 1)) * offsetPerPage
+      }
+      const freeSpace = availableSize - layout.width
+      const accumulatingOffset = interpolate(
+        index,
+        // Gradually shift every next item to the left so that the first item
+        // is positioned like "left: 0" but the last item is like "right: 0".
+        [0, itemsLength - 1],
+        [0, freeSpace],
+        'clamp',
+      )
+      return layout.x - accumulatingOffset
+    },
+    [itemsLength, contentSize, containerSize, layouts],
+  )
+
+  const progressToOffset = useCallback(
+    (progress: number) => {
+      'worklet'
+      return interpolate(
+        progress,
+        [Math.floor(progress), Math.ceil(progress)],
+        [
+          indexToOffset(Math.floor(progress)),
+          indexToOffset(Math.ceil(progress)),
+        ],
+        'clamp',
+      )
+    },
+    [indexToOffset],
+  )
+
+  // When we know the entire layout for the first time, scroll selection into view.
+  useAnimatedReaction(
+    () => layouts.get().length,
+    (nextLayoutsLength, prevLayoutsLength) => {
+      if (nextLayoutsLength !== prevLayoutsLength) {
+        if (
+          nextLayoutsLength === itemsLength &&
+          didInitialScroll.get() === false
+        ) {
+          didInitialScroll.set(true)
+          const progress = dragProgress.get()
+          const offset = progressToOffset(progress)
+          // It's unclear why we need to go back to JS here. It seems iOS-specific.
+          runOnJS(scrollToOffsetJS)(offset)
+        }
+      }
+    },
+  )
+
+  // When you swipe the pager, the tabbar should scroll automatically
+  // as you're dragging the page and then even during deceleration.
+  useAnimatedReaction(
+    () => dragProgress.get(),
+    (nextProgress, prevProgress) => {
+      if (
+        nextProgress !== prevProgress &&
+        dragState.value !== 'idle' &&
+        // This is only OK to do when we're 100% sure we're synced.
+        // Otherwise, there would be a jump at the beginning of the swipe.
+        syncScrollState.get() === 'synced'
+      ) {
+        const offset = progressToOffset(nextProgress)
+        scrollTo(scrollElRef, offset, 0, false)
+      }
+    },
+  )
+
+  // If the syncing is currently off but you've just finished swiping,
+  // it's an opportunity to resync. It won't feel disruptive because
+  // you're not directly interacting with the tabbar at the moment.
+  useAnimatedReaction(
+    () => dragState.value,
+    (nextDragState, prevDragState) => {
+      if (
+        nextDragState !== prevDragState &&
+        nextDragState === 'idle' &&
+        (syncScrollState.get() === 'unsynced' ||
+          syncScrollState.get() === 'needs-sync')
+      ) {
+        const progress = dragProgress.get()
+        const offset = progressToOffset(progress)
+        scrollTo(scrollElRef, offset, 0, true)
+        syncScrollState.set('synced')
+      }
+    },
+  )
+
+  // When you press on the item, we'll scroll into view -- unless you previously
+  // have scrolled the tabbar manually, in which case it'll re-sync on next press.
+  const onPressUIThread = useCallback(
+    (index: number) => {
+      'worklet'
+      const itemLayout = layouts.get()[index]
+      if (!itemLayout) {
+        // Should not happen.
+        return
+      }
+      const leftEdge = itemLayout.x - OFFSCREEN_ITEM_WIDTH
+      const rightEdge = itemLayout.x + itemLayout.width + OFFSCREEN_ITEM_WIDTH
+      const scrollLeft = scrollX.get()
+      const scrollRight = scrollLeft + containerSize.get()
+      const scrollIntoView = leftEdge < scrollLeft || rightEdge > scrollRight
+      if (
+        syncScrollState.get() === 'synced' ||
+        syncScrollState.get() === 'needs-sync' ||
+        scrollIntoView
+      ) {
+        const offset = progressToOffset(index)
+        scrollTo(scrollElRef, offset, 0, true)
+        syncScrollState.set('synced')
+      } else {
+        // The item is already in view so it's disruptive to
+        // scroll right now. Do it on the next opportunity.
+        syncScrollState.set('needs-sync')
+      }
+    },
+    [
+      syncScrollState,
+      scrollElRef,
+      scrollX,
+      progressToOffset,
+      containerSize,
+      layouts,
+    ],
+  )
+
+  const onItemLayout = useCallback(
+    (i: number, layout: {x: number; width: number}) => {
+      'worklet'
+      layouts.modify(ls => {
+        ls[i] = layout
+        return ls
+      })
+    },
+    [layouts],
+  )
+
+  const onTextLayout = useCallback(
+    (i: number, layout: {width: number}) => {
+      'worklet'
+      textLayouts.modify(ls => {
+        ls[i] = layout
+        return ls
+      })
+    },
+    [textLayouts],
+  )
+
+  const indicatorStyle = useAnimatedStyle(() => {
+    if (!_WORKLET) {
+      return {opacity: 0}
+    }
+    const layoutsValue = layouts.get()
+    const textLayoutsValue = textLayouts.get()
+    if (
+      layoutsValue.length !== itemsLength ||
+      textLayoutsValue.length !== itemsLength
+    ) {
+      return {
+        opacity: 0,
+      }
+    }
+
+    function getScaleX(index: number) {
+      const textWidth = textLayoutsValue[index].width
+      const itemWidth = layoutsValue[index].width
+      const minIndicatorWidth = 45
+      const maxIndicatorWidth = itemWidth - 2 * CONTENT_PADDING
+      const indicatorWidth = Math.min(
+        Math.max(minIndicatorWidth, textWidth),
+        maxIndicatorWidth,
+      )
+      return indicatorWidth / contentSize.get()
+    }
+
+    if (textLayoutsValue.length === 1) {
+      return {
+        opacity: 1,
+        transform: [
+          {
+            scaleX: getScaleX(0),
+          },
+        ],
+      }
+    }
+    return {
+      opacity: 1,
+      transform: [
+        {
+          translateX: interpolate(
+            dragProgress.get(),
+            layoutsValue.map((l, i) => {
+              'worklet'
+              return i
+            }),
+            layoutsValue.map(l => {
+              'worklet'
+              return l.x + l.width / 2 - contentSize.get() / 2
+            }),
+          ),
+        },
+        {
+          scaleX: interpolate(
+            dragProgress.get(),
+            textLayoutsValue.map((l, i) => {
+              'worklet'
+              return i
+            }),
+            textLayoutsValue.map((l, i) => {
+              'worklet'
+              return getScaleX(i)
+            }),
+          ),
+        },
+      ],
+    }
+  })
+
+  const onPressItem = useCallback(
+    (index: number) => {
+      runOnUI(onPressUIThread)(index)
+      onSelect?.(index)
+      if (index === selectedPage) {
+        onPressSelected?.(index)
+      }
+    },
+    [onSelect, selectedPage, onPressSelected, onPressUIThread],
+  )
+
+  return (
+    <View
+      testID={testID}
+      style={[t.atoms.bg, a.flex_row]}
+      accessibilityRole="tablist">
+      <BlockDrawerGesture>
+        <ScrollView
+          testID={`${testID}-selector`}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          ref={scrollElRef}
+          contentContainerStyle={styles.contentContainer}
+          onLayout={e => {
+            containerSize.set(e.nativeEvent.layout.width)
+          }}
+          onScrollBeginDrag={() => {
+            // Remember that you've manually messed with the tabbar scroll.
+            // This will disable auto-adjustment until after next pager swipe or item tap.
+            syncScrollState.set('unsynced')
+          }}
+          onScroll={e => {
+            scrollX.value = Math.round(e.nativeEvent.contentOffset.x)
+          }}>
+          <Animated.View
+            onLayout={e => {
+              contentSize.set(e.nativeEvent.layout.width)
+            }}
+            style={{flexDirection: 'row', flexGrow: 1}}>
+            {items.map((item, i) => {
+              return (
+                <TabBarItem
+                  key={i}
+                  index={i}
+                  testID={testID}
+                  dragProgress={dragProgress}
+                  item={item}
+                  onPressItem={onPressItem}
+                  onItemLayout={onItemLayout}
+                  onTextLayout={onTextLayout}
+                />
+              )
+            })}
+            <Animated.View
+              style={[
+                indicatorStyle,
+                {
+                  position: 'absolute',
+                  left: 0,
+                  bottom: 0,
+                  right: 0,
+                  borderBottomWidth: 2,
+                  borderColor: t.palette.primary_500,
+                },
+              ]}
+            />
+          </Animated.View>
+        </ScrollView>
+      </BlockDrawerGesture>
+      <View style={[t.atoms.border_contrast_low, styles.outerBottomBorder]} />
+    </View>
+  )
+}
+
+function TabBarItem({
+  index,
+  testID,
+  dragProgress,
+  item,
+  onPressItem,
+  onItemLayout,
+  onTextLayout,
+}: {
+  index: number
+  testID: string | undefined
+  dragProgress: SharedValue<number>
+  item: string
+  onPressItem: (index: number) => void
+  onItemLayout: (index: number, layout: {x: number; width: number}) => void
+  onTextLayout: (index: number, layout: {width: number}) => void
+}) {
+  const t = useTheme()
+  const style = useAnimatedStyle(() => {
+    if (!_WORKLET) {
+      return {opacity: 0.7}
+    }
+    return {
+      opacity: interpolate(
+        dragProgress.get(),
+        [index - 1, index, index + 1],
+        [0.7, 1, 0.7],
+        'clamp',
+      ),
+    }
+  })
+
+  const handleLayout = useCallback(
+    (e: LayoutChangeEvent) => {
+      runOnUI(onItemLayout)(index, e.nativeEvent.layout)
+    },
+    [index, onItemLayout],
+  )
+
+  const handleTextLayout = useCallback(
+    (e: LayoutChangeEvent) => {
+      runOnUI(onTextLayout)(index, e.nativeEvent.layout)
+    },
+    [index, onTextLayout],
+  )
+
+  return (
+    <View onLayout={handleLayout} style={{flexGrow: 1}}>
+      <PressableWithHover
+        testID={`${testID}-selector-${index}`}
+        style={styles.item}
+        hoverStyle={t.atoms.bg_contrast_25}
+        onPress={() => onPressItem(index)}
+        accessibilityRole="tab">
+        <Animated.View style={[style, styles.itemInner]}>
+          <Text
+            emoji
+            testID={testID ? `${testID}-${item}` : undefined}
+            style={[styles.itemText, t.atoms.text, a.text_md, a.font_bold]}
+            onLayout={handleTextLayout}>
+            {item}
+          </Text>
+        </Animated.View>
+      </PressableWithHover>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flexGrow: 1,
+    backgroundColor: 'transparent',
+    paddingHorizontal: CONTENT_PADDING,
+  },
+  item: {
+    flexGrow: 1,
+    paddingTop: 10,
+    paddingHorizontal: ITEM_PADDING,
+    justifyContent: 'center',
+  },
+  itemInner: {
+    alignItems: 'center',
+    flexGrow: 1,
+    paddingBottom: 10,
+    borderBottomWidth: 3,
+    borderBottomColor: 'transparent',
+  },
+  itemText: {
+    lineHeight: 20,
+    textAlign: 'center',
+  },
+  outerBottomBorder: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: '100%',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+})
