e656969797290c482056f00d653c6dca0461740b
zhengzhou <zhengzhou@rivtower.com>
2025-06-16 15:46:53 +0800

    chore: 文件扩展名调整

--- src/state/persisted/index.ts
+++ src/state/persisted/index.ts
@@ -1,13 +1,14 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import EventEmitter from 'eventemitter3'
 
+import BroadcastChannel from '#/lib/broadcast'
 import {logger} from '#/logger'
 import {
   defaults,
-  Schema,
+  type Schema,
   tryParse,
   tryStringify,
 } from '#/state/persisted/schema'
-import {PersistedApi} from './types'
+import {type PersistedApi} from './types'
 import {normalizeData} from './util'
 
 export type {PersistedAccount, Schema} from '#/state/persisted/schema'
@@ -15,10 +16,16 @@ export {defaults} from '#/state/persisted/schema'
 
 const BSKY_STORAGE = 'BSKY_STORAGE'
 
+const broadcast = new BroadcastChannel('BSKY_BROADCAST_CHANNEL')
+const UPDATE_EVENT = 'BSKY_UPDATE'
+
 let _state: Schema = defaults
+const _emitter = new EventEmitter()
 
 export async function init() {
-  const stored = await readFromStorage()
+  broadcast.onmessage = onBroadcastMessage
+  window.onstorage = onStorage
+  const stored = readFromStorage()
   if (stored) {
     _state = stored
   }
@@ -34,57 +41,123 @@ export async function write<K extends keyof Schema>(
   key: K,
   value: Schema[K],
 ): Promise<void> {
+  const next = readFromStorage()
+  if (next) {
+    // The storage could have been updated by a different tab before this tab is notified.
+    // Make sure this write is applied on top of the latest data in the storage as long as it's valid.
+    _state = next
+    // Don't fire the update listeners yet to avoid a loop.
+    // If there was a change, we'll receive the broadcast event soon enough which will do that.
+  }
+  try {
+    if (JSON.stringify({v: _state[key]}) === JSON.stringify({v: value})) {
+      // Fast path for updates that are guaranteed to be noops.
+      // This is good mostly because it avoids useless broadcasts to other tabs.
+      return
+    }
+  } catch (e) {
+    // Ignore and go through the normal path.
+  }
   _state = normalizeData({
     ..._state,
     [key]: value,
   })
-  await writeToStorage(_state)
+  writeToStorage(_state)
+  broadcast.postMessage({event: {type: UPDATE_EVENT, key}})
+  broadcast.postMessage({event: UPDATE_EVENT}) // Backcompat while upgrading
 }
 write satisfies PersistedApi['write']
 
 export function onUpdate<K extends keyof Schema>(
-  _key: K,
-  _cb: (v: Schema[K]) => void,
+  key: K,
+  cb: (v: Schema[K]) => void,
 ): () => void {
-  return () => {}
+  const listener = () => cb(get(key))
+  _emitter.addListener('update', listener) // Backcompat while upgrading
+  _emitter.addListener('update:' + key, listener)
+  return () => {
+    _emitter.removeListener('update', listener) // Backcompat while upgrading
+    _emitter.removeListener('update:' + key, listener)
+  }
 }
 onUpdate satisfies PersistedApi['onUpdate']
 
 export async function clearStorage() {
   try {
-    await AsyncStorage.removeItem(BSKY_STORAGE)
+    localStorage.removeItem(BSKY_STORAGE)
   } catch (e: any) {
-    logger.error(`persisted store: failed to clear`, {message: e.toString()})
+    // Expected on the web in private mode.
   }
 }
 clearStorage satisfies PersistedApi['clearStorage']
 
-async function writeToStorage(value: Schema) {
-  const rawData = tryStringify(value)
-  if (rawData) {
-    try {
-      await AsyncStorage.setItem(BSKY_STORAGE, rawData)
-    } catch (e) {
-      logger.error(`persisted state: failed writing root state to storage`, {
-        message: e,
-      })
+function onStorage() {
+  const next = readFromStorage()
+  if (next === _state) {
+    return
+  }
+  if (next) {
+    _state = next
+    _emitter.emit('update')
+  }
+}
+
+async function onBroadcastMessage({data}: MessageEvent) {
+  if (
+    typeof data === 'object' &&
+    (data.event === UPDATE_EVENT || // Backcompat while upgrading
+      data.event?.type === UPDATE_EVENT)
+  ) {
+    // read next state, possibly updated by another tab
+    const next = readFromStorage()
+    if (next === _state) {
+      return
+    }
+    if (next) {
+      _state = next
+      if (typeof data.event.key === 'string') {
+        _emitter.emit('update:' + data.event.key)
+      } else {
+        _emitter.emit('update') // Backcompat while upgrading
+      }
+    } else {
+      logger.error(
+        `persisted state: handled update update from broadcast channel, but found no data`,
+      )
     }
   }
 }
 
-async function readFromStorage(): Promise<Schema | undefined> {
+function writeToStorage(value: Schema) {
+  const rawData = tryStringify(value)
+  if (rawData) {
+    try {
+      localStorage.setItem(BSKY_STORAGE, rawData)
+    } catch (e) {
+      // Expected on the web in private mode.
+    }
+  }
+}
+
+let lastRawData: string | undefined
+let lastResult: Schema | undefined
+function readFromStorage(): Schema | undefined {
   let rawData: string | null = null
   try {
-    rawData = await AsyncStorage.getItem(BSKY_STORAGE)
+    rawData = localStorage.getItem(BSKY_STORAGE)
   } catch (e) {
-    logger.error(`persisted state: failed reading root state from storage`, {
-      message: e,
-    })
+    // Expected on the web in private mode.
   }
   if (rawData) {
-    const parsed = tryParse(rawData)
-    if (parsed) {
-      return normalizeData(parsed)
+    if (rawData === lastRawData) {
+      return lastResult
+    } else {
+      const result = tryParse(rawData)
+      if (result) {
+        lastRawData = rawData
+        lastResult = normalizeData(result)
+        return lastResult
+      }
     }
   }
 }
