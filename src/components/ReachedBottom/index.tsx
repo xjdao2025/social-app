fd7b50a30e31e56f677b6c73b31a9b5dbd3fc6f7
chenhui <274241365@qq.com>
2025-06-26 15:34:18 +0800

    fix: 到底了

---
+++ src/components/ReachedBottom/index.tsx
@@ -0,0 +1,27 @@
+import {StyleSheet, View} from 'react-native'
+
+import {Text} from '#/components/Typography'
+
+const BottomView = () => {
+  return (
+    <View style={styles.wrap}>
+      <Text style={styles.reach_bottom}>到底了~</Text>
+    </View>
+  )
+}
+
+export default BottomView
+
+const styles = StyleSheet.create({
+  wrap: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingBlock: 15,
+  },
+  reach_bottom: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#959399',
+  },
+})
--- src/view/com/posts/FollowingEndOfFeed.tsx
+++ src/view/com/posts/FollowingEndOfFeed.tsx
@@ -8,9 +8,10 @@ import {Trans} from '@lingui/macro'
 import {useNavigation} from '@react-navigation/native'
 
 import {usePalette} from '#/lib/hooks/usePalette'
-import {NavigationProp} from '#/lib/routes/types'
+import {type NavigationProp} from '#/lib/routes/types'
 import {s} from '#/lib/styles'
 import {isWeb} from '#/platform/detection'
+import ReachedBottom from '#/components/ReachedBottom'
 import {Button} from '../util/forms/Button'
 import {Text} from '../util/text/Text'
 
@@ -32,53 +33,55 @@ export function FollowingEndOfFeed() {
     navigation.navigate('Feeds')
   }, [navigation])
 
-  return (
-    <View
-      style={[
-        styles.container,
-        pal.border,
-        {minHeight: Dimensions.get('window').height * 0.75},
-      ]}>
-      <View style={styles.inner}>
-        <Text type="xl-medium" style={[s.textCenter, pal.text]}>
-          <Trans>
-            You've reached the end of your feed! Find some more accounts to
-            follow.
-          </Trans>
-        </Text>
-        <Button
-          type="inverted"
-          style={styles.emptyBtn}
-          onPress={onPressFindAccounts}>
-          <Text type="lg-medium" style={palInverted.text}>
-            <Trans>Find accounts to follow</Trans>
-          </Text>
-          <FontAwesomeIcon
-            icon="angle-right"
-            style={palInverted.text as FontAwesomeIconStyle}
-            size={14}
-          />
-        </Button>
+  return <ReachedBottom />
 
-        <Text type="xl-medium" style={[s.textCenter, pal.text, s.mt20]}>
-          <Trans>You can also discover new Custom Feeds to follow.</Trans>
-        </Text>
-        <Button
-          type="inverted"
-          style={[styles.emptyBtn, s.mt10]}
-          onPress={onPressDiscoverFeeds}>
-          <Text type="lg-medium" style={palInverted.text}>
-            <Trans>Discover new custom feeds</Trans>
-          </Text>
-          <FontAwesomeIcon
-            icon="angle-right"
-            style={palInverted.text as FontAwesomeIconStyle}
-            size={14}
-          />
-        </Button>
-      </View>
-    </View>
-  )
+  // return (
+  //   <View
+  //     style={[
+  //       styles.container,
+  //       pal.border,
+  //       {minHeight: Dimensions.get('window').height * 0.75},
+  //     ]}>
+  //     <View style={styles.inner}>
+  //       <Text type="xl-medium" style={[s.textCenter, pal.text]}>
+  //         <Trans>
+  //           You've reached the end of your feed! Find some more accounts to
+  //           follow.
+  //         </Trans>
+  //       </Text>
+  //       <Button
+  //         type="inverted"
+  //         style={styles.emptyBtn}
+  //         onPress={onPressFindAccounts}>
+  //         <Text type="lg-medium" style={palInverted.text}>
+  //           <Trans>Find accounts to follow</Trans>
+  //         </Text>
+  //         <FontAwesomeIcon
+  //           icon="angle-right"
+  //           style={palInverted.text as FontAwesomeIconStyle}
+  //           size={14}
+  //         />
+  //       </Button>
+  //
+  //       <Text type="xl-medium" style={[s.textCenter, pal.text, s.mt20]}>
+  //         <Trans>You can also discover new Custom Feeds to follow.</Trans>
+  //       </Text>
+  //       <Button
+  //         type="inverted"
+  //         style={[styles.emptyBtn, s.mt10]}
+  //         onPress={onPressDiscoverFeeds}>
+  //         <Text type="lg-medium" style={palInverted.text}>
+  //           <Trans>Discover new custom feeds</Trans>
+  //         </Text>
+  //         <FontAwesomeIcon
+  //           icon="angle-right"
+  //           style={palInverted.text as FontAwesomeIconStyle}
+  //           size={14}
+  //         />
+  //       </Button>
+  //     </View>
+  //   </View>
+  // )
 }
 const styles = StyleSheet.create({
   container: {
